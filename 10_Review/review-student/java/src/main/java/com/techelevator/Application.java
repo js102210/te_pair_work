bf5a610032a5f6bcd60ea6738cbe2066ca1db5cf
unknown <M.bradfordcarter@gmail.com>
2021-01-23 14:33:11 -0500

    partern work

--- 10_Review/review-student/java/src/main/java/com/techelevator/Application.java
+++ 10_Review/review-student/java/src/main/java/com/techelevator/Application.java
@@ -2,9 +2,11 @@ package com.techelevator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
 
 public class Application {
-
+    private List<Department> departments = new ArrayList<>();
+    private List<Employee> employees = new ArrayList<>();
     /**
      * The main entry point in the application
      * @param args
@@ -13,37 +15,22 @@ public class Application {
         Application app = new Application();
         app.run();
     }
-    private List<Department> departments = new ArrayList<>();
+
+
     private void run() {
         // create some departments
 
 
-        createDepartments(); {
-            Department marketing = new Department(1, "Marketing");
-            departments.add(marketing);
-            Department sales = new Department(2, "Sales");
-            departments.add(sales);
-            Department engineering = new Department(3, "Engineering");
-            departments.add(engineering);
-
-        }
+        createDepartments();
 
         // print each department by name
-        printDepartments(); {
-
-            for (Department department : departments) {
-                System.out.println(department.getName());
-            }
-        }
+        printDepartments();
 
         // create employees
         createEmployees();
-
         // give Angie a 10% raise, she is doing a great job!
-
         // print all employees
         printEmployees();
-
         // create the TEams project
         createTeamsProject();
         // create the Marketing Landing Page Project
@@ -56,31 +43,57 @@ public class Application {
     /**
      * Create departments and add them to the collection of departments
      */
+
+
     private void createDepartments() {
+        Department marketing = new Department(1, "Marketing");
+        departments.add(marketing);
+        Department sales = new Department(2, "Sales");
+        departments.add(sales);
+        Department engineering = new Department(3, "Engineering");
+        departments.add(engineering);
+
     }
 
     /**
      * Print out each department in the collection.
      */
     private void printDepartments() {
-        System.out.println("------------- DEPARTMENTS ------------------------------");
-
+        System.out.println("\n------------- Departments ------------------------------");
+        for (Department department : departments) {
+            System.out.println(department.getName());
+        }
     }
 
     /**
      * Create employees and add them to the collection of employees
      */
     private void createEmployees() {
+            Employee Dean = new Employee();
+            Dean.setEmployeeId(001);
+            Dean.setFirstName("Dean");
+            Dean.setLastName("Johnson");
+            Dean.setEmail("djohnson@teams.com");
+            Dean.setDepartment(departments.get(2));
+            Dean.setHireDate("08/21/2020");
+            employees.add(Dean);
 
+            Employee Angie = new Employee(002,"Angie", "Smith", "asmith@teams.com", departments.get(2), "08/21/2020" );
+            employees.add(Angie);
+            Employee Margaret = new Employee(003, "Margaret", "Thompson", "mthompson@teams.com", departments.get(0), "08/21/2020" );
+            employees.add(Margaret);
+
+            Angie.raiseSalary(10);
     }
-
     /**
      * Print out each employee in the collection.
      */
     private void printEmployees() {
-        System.out.println("\n------------- EMPLOYEES ------------------------------");
-
-    }
+            System.out.println("\n------------- Employees ------------------------------");
+            for (Employee employee : employees) {
+                System.out.println(employee.getFullName()+ " " + employee.getSalary());
+            }
+        }
 
     /**
      * Create the 'TEams' project.
--- 10_Review/review-student/java/src/main/java/com/techelevator/Employee.java
+++ 10_Review/review-student/java/src/main/java/com/techelevator/Employee.java
@@ -11,26 +11,32 @@ public class Employee {
 
     //getters
     public long getEmployeeId() {
+
         return this.getEmployeeId();
     }
 
     public String getFirstName() {
+
         return this.firstName;
     }
 
     public String lastName() {
+
         return this.lastName;
     }
 
     public String getEmail() {
+
         return this.email;
     }
 
     public double getSalary() {
+
         return this.salary;
     }
 
     public Department getDepartment() {
+
         return this.department;
     }
 
@@ -44,33 +50,25 @@ public class Employee {
     public void setEmployeeId(long employeeId) {
         this.employeeId = employeeId;
     }
-
     public void setFirstName (String firstName) {
         this.firstName = firstName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public void setEmail (String email) {
         this.email = email;
     }
-
     public void setSalary (double salary) {
         this.salary = salary;
     }
     public void setDepartment (Department department) {
         this.department = department;
     }
-
     public void setHireDate (String hireDate) {
         this.hireDate = hireDate;
     }
-
-
     //constructors
-
     public Employee(long employeeId, String firstName, String lastName, String email, Department department, String hireDate) {
         setEmployeeId(employeeId);
         setFirstName(firstName);
@@ -79,18 +77,13 @@ public class Employee {
         setDepartment(department);
         setHireDate(hireDate);
     }
-
     public Employee () {
-
     }
-
     //other methods
     public String getFullName() {
         return this.lastName + ", " + this.firstName;
     }
-
     public void raiseSalary(double percent) {
         this.salary = this.salary + (this.salary * (percent/100));
     }
-
 }
